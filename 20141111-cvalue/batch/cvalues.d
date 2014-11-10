de100aac56b391d40fe1872245c375ce951ef4e2
majiang <neuron.ohori@gmail.com>
2014-11-10 18:05:40 +0900

    the table of c for (s, m)

---
+++ 20141111-cvalue/batch/cvalues.d
@@ -0,0 +1,44 @@
+import std.stdio;
+import std.path;
+import std.string;
+import std.conv : to;
+
+enum n = 32;
+
+void main(string[] args)
+{
+	if (args.length < 3)
+		return stderr.writeln("cvalues mmin mmax");
+	immutable
+		mmin = args[1].to!size_t(),
+		mmax = args[2].to!size_t(),
+		smin = 4,
+		smax = 17;
+	auto
+		out_csv = File("..".buildPath("cvalues", "cvalues.csv"), "w"),
+		out_tex = File("..".buildPath("cvalues", "cvalues.tex"), "w");
+	foreach (m; mmin..mmax)
+	{
+		out_csv.write(m);
+		out_tex.writef("$%s$", m);
+		foreach (s; smin..smax)
+		{
+			auto data_file = File("..".buildPath("..", "20141022-dynamic", "pointsets", "s%02d-m%02d-n%02d.csv".format(s, m, n)));
+			auto c = tryReadCvalue(data_file);
+			out_csv.writef(",%s", c);
+			out_tex.writef(" & $ %s$", c); //additional space is for c = "" case.
+		}
+		out_csv.writeln();
+		out_tex.writeln(" \\\\");
+	}
+	out_tex.write("\\hline");
+}
+
+auto tryReadCvalue(File file)
+{
+	try
+		return "%.2f".format(file.readln().strip().split(",")[1].to!real());
+	catch
+		return "";
+}
+
