90657505ce0fbe4dc62d64fa3a752ce57c120b45
majiang <neuron.ohori@gmail.com>
2014-12-05 11:46:16 +0900

    Add comments to nx-join.d

--- 20141117-nxseq-err/join.d
+++ 20141117-nxseq-err/join.d
@@ -9,14 +9,14 @@ void main()
 				immutable c = (i == 4 ? 10 : 1) * genz_c;
 				auto out_file = File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s, 8, 23, genz_c, i), "w");
 				foreach (in_file; [
-					File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s,  8, 17, c, i))
-				,	File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s, 17, 23, c, i))
-			//	,	File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s, 23, 30, c, i))
+					File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s,  8, 17, c, i)) // small
+				,	File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s, 17, 23, c, i)) // medium
+			//	,	File("s%02d-m%02dm%02d-%dgenz%d.csv".format(s, 23, 30, c, i)) // large
 				])
 				{
 					foreach (line; in_file.byLine())
 						out_file.writeln(line.strip());
-					in_file.close();
+					in_file.close(); // necessary to avoid `too many open files` error.
 				}
 			}
 }
